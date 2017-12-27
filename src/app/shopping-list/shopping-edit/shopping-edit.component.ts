c481e0b3262204a8e6392158fac72ba4f507b5ef
AlfredWu18 <yuw681@eng.ucsd.edu>
2017-12-26 23:25:10 -0800

    Loading the shopping list to the form

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit, ViewChild} from '@angular/core';
 import {NgForm} from '@angular/forms';
 import {Ingredient} from '../../shared/ingredient.model';
 import {ShoppingListService} from '../shopping-list.service';
@@ -10,9 +10,11 @@ import {Subscription} from 'rxjs/Subscription';
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent implements OnInit, OnDestroy {
+  @ViewChild('f') slForm: NgForm;
   subscription: Subscription;
   editMode = false;
   editItemIndex: number;
+  editItem: Ingredient;
 
   constructor(private slService: ShoppingListService) { }
 
@@ -21,6 +23,11 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
       .subscribe((index: number) => {
           this.editItemIndex = index;
           this.editMode = true;
+          this.editItem = this.slService.getIngredient(index);
+          this.slForm.setValue({
+            name: this.editItem.name,
+            amount: this.editItem.amount
+          });
       });
   }
 
