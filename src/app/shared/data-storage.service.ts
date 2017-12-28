8639ae6cfc63a35fe9ba43387094e1847ac9c353
AlfredWu18 <yuw681@eng.ucsd.edu>
2017-12-28 09:48:09 -0800

    Transform response data to prevent errors

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,7 +1,8 @@
 import {Injectable} from '@angular/core';
 import {RecipeService} from '../recipes/recipe.service';
 import {Http, Response} from '@angular/http';
-import {Recipe} from "../recipes/recipe.model";
+import {Recipe} from '../recipes/recipe.model';
+import 'rxjs/add/operator/map';
 
 @Injectable()
 export class DataStorageService {
@@ -11,8 +12,19 @@ export class DataStorageService {
   }
   getRecipes() {
     this.http.get('https://foodie-alfred.firebaseio.com/recipes.json')
-      .subscribe((response: Response) => {
-        const recipes: Recipe[] = response.json();
+      .map(
+        (response: Response) => {
+          const recipes: Recipe[] = response.json();
+          for (let recipe of recipes) {
+            if (!recipe['ingredients']) {
+              console.log(recipe);
+              recipe['ingredients'] = [];
+            }
+          }
+          return recipes;
+        }
+      )
+      .subscribe((recipes: Recipe[]) => {
         this.recipeService.setRecipes(recipes);
       });
   }
