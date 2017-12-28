db8b88a88b9386a2ad30cead0d65c9cbc5bfb960
AlfredWu18 <yuw681@eng.ucsd.edu>
2017-12-28 10:29:48 -0800

    create signup page & route and set up the firebase SDK

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   loadedFeature = 'recipe';
 
+  ngOnInit() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyB5Bqe8DmyGUrjotX7Qsuxc_S7FRrrMjyY',
+      authDomain: 'foodie-alfred.firebaseapp.com'
+    });
+  }
   onNavigate(feature: string) {
     this.loadedFeature = feature;
   }
