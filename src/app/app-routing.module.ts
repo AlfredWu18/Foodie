129229741faab09f6a97898513173eec5690ec08
AlfredWu18 <yuw681@eng.ucsd.edu>
2017-12-30 00:38:23 -0800

    create a basic core module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,11 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 
 import {ShoppingListComponent} from './shopping-list/shopping-list.component';
-import {HomeComponent} from './home/home.component';
+import {HomeComponent} from './core/home/home.component';
 
 const appRoutes: Routes = [
   { path: '', component: HomeComponent},
-  { path: 'recipes', loadChildren: './recipes/recipes.module#RecipeModule'},
+  { path: 'recipes', loadChildren: './recipes/recipes.module#RecipesModule'},
   { path: 'shopping-list', component: ShoppingListComponent}
 ];
 
